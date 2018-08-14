3ae49d399f3baeac6124ca414d775956dc24cc7d
Gary <gary@Joannes-MacBook-Pro.local>
2018-08-14 13:22:58 -0700

    add deepvoice

---
+++ hparams.py
@@ -0,0 +1,13 @@
+class hparams:
+    num_mels = 80
+    fmin = 125
+    fmax = 7600
+    fft_size = 2048
+    hop_size = 256
+    sample_rate = 22050
+    preemphasis = 0.97 # set to 0 if want to disable
+    min_level_db = -100
+    ref_level_db = 20
+    rescaling = False
+    rescaling_max = 0.999
+    allow_clipping_in_normalization = True
