96a166c24950f629e8f344bbb16a710d86573c01
G-Wang <ywa289@sfu.ca>
2019-03-29 10:56:21 -0700

    fix quantization

--- preprocess.py
+++ preprocess.py
@@ -26,10 +26,10 @@ def get_wav_mel(path):
         return wav.astype(np.float32), mel
     elif hp.input_type == 'mulaw':
         quant = encode_mu_law(wav, 2**hp.bits)
-        return quant.astype(np.int), mel
+        return quant.astype(np.int16), mel
     elif hp.input_type == 'bits':
-        quant = quant_2_float(wav, hp.bits)
-        return quant.astype(np.int), mel
+        quant = float_2_quant(wav, hp.bits)
+        return quant.astype(np.int16), mel
     else:
         raise ValueError("hp.input_type {} not recognized".format(hp.input_type))
 
